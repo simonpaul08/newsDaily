3e65b9f47a116d5b609e1ef9353b31bd189dd1cd
Simon <devlancer.work@gmail.com>
2022-09-17 17:37:56 +0530

    commit 2

--- src/components/News.js
+++ src/components/News.js
@@ -4,6 +4,7 @@ import NewsItem from './NewsItem'
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
+
 const News = (props) => {
 
     const [articles, setarticles] = useState([])
